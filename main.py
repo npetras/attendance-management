bfa813f246cf94aa36d111b60d9f4836f02520b4
Nicolas Petras <nicolaspetras@gmail.com>
2022-06-15 10:06:22 +0100

    updated admin functions for the 1st 4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from admin import *
+from nick_admin import *
 
 import textwrap
 
@@ -7,6 +7,32 @@ USER = 2
 EXIT_1 = 3
 EXIT_ADMIN = 10
 EXIT_USER = 10
+
+
+def initialise(admin):
+    admin.add_grade()
+    admin.add_grade()
+
+    admin.add_section(1)
+    admin.add_section(1)
+    admin.add_section(1)
+    admin.add_section(2)
+
+    stud1 = Student("Nicolas", 15)
+    stud2 = Student("Caoimhe", 16)
+    stud3 = Student("Morgan", 14)
+    stud4 = Student("Bill", 17)
+
+    admin.add_student_to_class(stud1, 1)
+    admin.add_student_to_class(stud2, 1)
+    admin.add_student_to_class(stud3, 1)
+    admin.add_student_to_class(stud4, 2)
+
+    staff1 = Staff("John", "ICT")
+    staff2 = Staff("Lana", "English")
+
+    admin.allocate_staff_to_section(staff1, 1, 1)
+    admin.allocate_staff_to_section(staff1, 1, 2)
 
 
 def admin_menu(admin):
@@ -18,13 +44,53 @@ def admin_menu(admin):
     5. Take Student Attendance
     6. Print Student Attendance
     """
+
+    admin.display()
     admin_choice = int(input(textwrap.dedent(choices)))
+
+    # add grade
     if admin_choice == 1:
-        grade = Grade()
-        admin.add_grade(grade)
+        admin.add_grade()
+    # add section under grade
     elif admin_choice == 2:
         grade = int(input("Grade to add the section under: "))
         admin.add_section(grade)
+    # add student to grade
+    elif admin_choice == 3:
+        print("Please enter student details")
+        name = input("Student name: ")
+        age = int(input("Student age: "))
+        grade = int(input("Grade: "))
+        student = Student(name, age)
+        admin.add_student_to_class(student, grade)
+    # allocate staff to grade section
+    elif admin_choice == 4:
+        print("Please enter Staff details")
+        name = input("Student name: ")
+        department = input("Staff department: ")
+        staff = Staff(name, department)
+        grade_id = int(input("Grade: "))
+        section_id = int(input("Section: "))
+        admin.allocate_staff_to_section(staff, grade_id, section_id)
+    # take student attendance for today
+    elif admin_choice == 5:
+        # enter staff id
+        staff_id = int(input("Enter your Staff ID: "))
+        # print all students for the Grade Section
+        section_iterator = filter(lambda s: s.staff.id == staff_id, admin.grades.sections)
+        grade_list = list(section_iterator)
+        grade_list[0].displayStudents()
+
+        # ask which ones are absent, enter 0 for no absences
+        # for all absent ids
+        # mark student as absent
+        # for all non-absent ids
+        # mark them as present
+        pass
+    # print attendance for today
+    elif admin_choice == 6:
+        admin.display_last_student_attendance()
+        pass
     else:
         print("Invalid choice, try again: ")
 
@@ -36,7 +102,7 @@ def user_menu():
 if __name__ == '__main__':
 
     admin = Admin()
-
+    initialise(admin)
     user_admin_choice = int(input("1. Admin \t 2. User \t 3. Exit\n"))
 
     while user_admin_choice != EXIT_1:
--- nick_admin.py
+++ nick_admin.py
@@ -1,44 +1,114 @@
 import itertools
-from string import ascii_lowercase
+
+
+class InvalidSectionId(Exception):
+    pass
+
+
+def int_id_to_letter(int_id):
+    int_to_letter = {
+        1: "a",
+        2: "b",
+        3: "c",
+        4: "d",
+        5: "e",
+        6: "f",
+        7: "g",
+        8: "h",
+        9: "i",
+        10: "k"
+    }
+    if 0 < int_id <= 10:
+        return int_to_letter[int_id]
+    else:
+        raise InvalidSectionId(f"Invalid ID {int_id} was provided")
 
 
 class Grade:
+    id_iter = itertools.count(1)
     max_students_per_section = 20
 
     def __init__(self):
+        self.id = next(self.id_iter)
         self.sections = list()
 
     def add_section(self, section):
         self.sections.append(section)
 
+    def add_student(self, student):
+        # fill current section up, before creating a new one
+        if len(self.sections[-1].students) < self.max_students_per_section:
+            self.sections[-1].add_student(student)
+        else:
+            new_section = GradeSection()
+            new_section.add_student(student)
+            self.sections.append(new_section)
+
+    def display(self):
+        print(self.id, end="\t")
+        print(self.max_students_per_section, end="\t")
+        print(len(self.sections), end="\t")
+        for section in self.sections:
+            print(f"{int_id_to_letter(section.id)},", end="")
+
 
 class GradeSection:
     id_iter = itertools.count(1)
 
-    def __init__(self, grade, staff):
-        self.section_id = next(self.id_iter)
-        self.grade = grade
+    def __init__(self, staff=None):
+        self.id = next(self.id_iter)
         self.students = list()
         self.staff = staff
 
     def add_student(self, student):
         self.students.append(student)
 
+    def display_students(self):
+        print("Students:")
+        for student in self.students:
+            print(student.display())
+
     def display(self):
-        pass
-        # print(section)
+        print(int_id_to_letter(self.id))
+        try:
+            print("Teacher: ", end="")
+            print(self.staff.display())
+        except AttributeError:
+            print("Not yet allocated")
+
+        self.display_students()
 
 
-class Student:
-    id_iter = itertools.count(1)
-
+class Person:
     def __init__(self, name):
-        self.id = next(self.id_iter)
         self.name = name
 
+    def display(self):
+        print(f"Name: {self.name}", end=" ")
 
-class Staff:
-    pass
+
+class Student(Person):
+    id_iter = itertools.count(1)
+
+    def __init__(self, name, age):
+        self.id = next(self.id_iter)
+        super().__init__(name)
+        self.age = age
+
+    def display(self):
+        print(f"ID: {self.id}", f"Name: {self.name}", f"Age: {self.age}",  end=" ")
+
+
+class Staff(Person):
+    id_iter = itertools.count(1)
+
+    def __init__(self, name, department):
+        self.id = next(self.id_iter)
+        super().__init__(name)
+        self.department = department
+
+    def display(self):
+        print(f"ID: {self.id} {self.name} from {self.department}", end=" ")
 
 
 class Admin:
@@ -47,17 +117,40 @@ class Admin:
         self.grades = list()
 
     # add classes
-    def add_grade(self, grade):
-        print("Adding grade")
+    def add_grade(self):
+        # print("Adding grade")
         self.grades.append(Grade())
 
-    def add_section(self, grade):
-        print("Adding section")
-        # grade_filter = filter(lambda student: if )
+    def add_section(self, grade_id):
+        # print(f"Adding section to grade {grade_id}")
+        grade_iterator = filter(lambda g: g.id == grade_id, self.grades)
+        grade_list = list(grade_iterator)
+        section = GradeSection()
+        grade_list[0].add_section(section)
 
-    def add_student_to_class(self, student, student_class):
+    def add_student_to_class(self, student, grade_id):
+        grade_iterator = filter(lambda g: g.id == grade_id, self.grades)
+        grade_list = list(grade_iterator)
+        grade_list[0].add_student(student)
+
+    def allocate_staff_to_section(self, staff, grade_id, section_id):
+        grade_iterator = filter(lambda g: g.id == grade_id, self.grades)
+        grade_list = list(grade_iterator)
+        section_iterator = filter(lambda s: s.id == section_id, grade_list[0].sections)
+        section_list = list(section_iterator)
+        section_list[0].staff = staff
+
+    def print_last_student_attendance(self):
         pass
 
+    def display(self):
+        print("Grade \t Max Students in Section \t No. of Sections \t Sections")
+        for grade in self.grades:
+            print(grade.display())
+        print("Sections: ")
+        for grade in self.grades:
+            for section in grade.sections:
+                section.display()
     # add sections
     # add students to classes
     # allocate staff to classes
